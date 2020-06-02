4602bbc18033c89975f9051afe45871d1e32aca7
vitonittwo <vorslvv33@inbox.lv>
2020-06-02 11:50:53 +0300

    add Multiply letter

---
+++ src/taskMultiplication.java
@@ -0,0 +1,18 @@
+public class taskMultiplication {
+
+    public static void main(String[] args) {
+
+        for (int b = 1; b < 11; b++) {
+            System.out.print("\n");
+            for (int a = 1; a < 11; a++) {
+
+            System.out.print(a + " x " + b + " = " + a*b + "    ");
+
+
+             }
+
+       }
+
+    }
+}
+
